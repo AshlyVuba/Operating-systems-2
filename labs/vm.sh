c9ace134aef9c83b165b35bda9fc1034c57500d8
AshlyVuba <aphiwe.avuba@gmail.com>
2023-10-20 10:44:27 +0200

    Update vm.sh

--- labs/vm.sh
+++ labs/vm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 az vm create \
-  --resource-group learn-4ea6852d-73d2-41ec-8c22-1d3e6498b73d \
+  --resource-group Ops2\
   --name my-vm \
   --public-ip-sku Standard \
   --image Ubuntu2204 \
